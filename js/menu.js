f0ad62aee4a08ea443b106efe1f3f26fb2b184cb
Junior Santos <gilsonsantosjunior02@gmail.com>
2022-03-14 22:35:55 -0300

    commit site

--- js/menu.js
+++ js/menu.js
@@ -3,20 +3,23 @@
 const btnOpenMenu = document.getElementById('btnOpenMenu'); 
 
 btnOpenMenu.addEventListener('click', function() {
+    // conteiner
     let menu = document.getElementById('menu'); 
+    // itens 
+    let itensMenu = document.getElementById('itensMenu'); 
+    // conteiner 
+    let conteinerDiv = document.getElementById('pagePrincipal'); 
 
-    /*function imagens */
-    function diminuirImgJs (img) {
-    let imgHeaderJS = document.getElementById('imgHeader').style.width = img
-}
-
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none'; 
+    // show
+    if (itensMenu.style.display === 'block') {
+        itensMenu.style.display = 'none'; 
+        menu.style.width = '0'
+        conteinerDiv.style.display = 'block'
         btnOpenMenu.innerHTML = '&#9776;'
-        diminuirImgJs('30px')
     } else {
-        menu.style.display = 'block'; 
+        itensMenu.style.display = 'block'; 
+        conteinerDiv.style.display = 'none'
+        menu.style.width = '100%'
         btnOpenMenu.innerHTML = '&#10008;'
-        diminuirImgJs('36px')
     }
 }); 
